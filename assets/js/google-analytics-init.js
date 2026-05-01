4eca29cad78c5fa7cd5ee8708c46af3ae0ec7ac5
Sergey Polyarus <57401230+icreated@users.noreply.github.com>
2026-05-01 14:06:49 +0200

    fix: google cookie

--- assets/js/google-analytics-init.js
+++ assets/js/google-analytics-init.js
@@ -2,5 +2,7 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'G-M5FFVQXXYY');
+gtag('config', 'G-M5FFVQXXYY', {
+  cookie_flags: 'SameSite=None;Secure'
+});
 //# sourceMappingURL=google-analytics-init.js.map
